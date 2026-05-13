7cc00ab7b82a335057156c50b1242a5e03c0314d
i3oss <bossxlp@gmail.com>
2026-05-13 14:48:59 +0700

    feat: add dag orchestrate to example

---
+++ dags/dag_orchestrate_2.py
@@ -0,0 +1,37 @@
+import os
+
+import pendulum
+from airflow.sdk import dag, task
+
+
+@dag(
+    dag_id="dag_orchestrate_2",
+)
+def dag_orchestrate_2():
+
+    @task.python
+    def first_task():
+        print("This is the first task")
+
+    @task.python
+    def second_task():
+        print("This is the second task")
+
+    @task.python
+    def third_task():
+        os.makedirs(os.path.dirname("/opt/airflow/logs/data"), exist_ok=True)
+
+        # Simulate data fetching and write to a file
+        with open("/opt/airflow/logs/data/output_2.txt", "w") as f:
+            f.write(f"Data procressed at {pendulum.now('Asia/Bangkok')}\n")
+
+    # Defining task dependencies
+    first = first_task()
+    second = second_task()
+    third = third_task()
+
+    first >> second >> third
+
+
+# Instantiating the DAG
+dag_orchestrate_2()
---
+++ dags/dag_orchestrate_parent.py
@@ -0,0 +1,49 @@
+from posix import wait
+
+from airflow.providers.standard.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.sdk import dag, task
+from dag_orchestrate_1 import dag_orchestrate_1
+from dag_orchestrate_2 import dag_orchestrate_2
+
+### this code is show it not work it need to use TriggerDagRunOperator to trigger the child DAGs instead of calling the DAG functions directly
+# @dag(
+#     dag_id="dag_orchestrate_parent",
+# )
+# def dag_orchestrate_parent():
+
+#     @task
+#     def first_orchestator_task():
+#         return dag_orchestrate_1()
+
+#     @task
+#     def second_orchestrator_task():
+#         return dag_orchestrate_2()
+
+#     first_orchestator_task() >> second_orchestrator_task()
+
+
+# # Instantiating the DAG
+# dag_orchestrate_parent()
+#
+@dag(
+    dag_id="dag_orchestrate_parent",
+)
+def dag_orchestrate_parent():
+
+    trigger_dag_1 = TriggerDagRunOperator(
+        task_id="trigger_dag_orchestrate_1",
+        trigger_dag_id="dag_orchestrate_1",  # The DAG ID to trigger
+        wait_for_completion=True,  # Wait for the triggered DAG to complete before proceeding -> after add this it change from async to sync
+    )
+
+    trigger_dag_2 = TriggerDagRunOperator(
+        task_id="trigger_dag_orchestrate_2",
+        trigger_dag_id="dag_orchestrate_2",  # The DAG ID to trigger
+        wait_for_completion=True,  # Wait for the triggered DAG to complete before proceeding
+    )
+
+    trigger_dag_1 >> trigger_dag_2
+
+
+# Instantiating the DAG
+dag_orchestrate_parent()
